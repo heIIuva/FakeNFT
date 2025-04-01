c3f5cac4eb07ad43bccac5a20e75eadaf1f7bc80
ultra <presenceaura@gmail.com>
2025-04-01 17:27:29 +0300

    added pay button and labels

--- FakeNFT/Scenes /CartFlow/Cart/CartViewController.swift
+++ FakeNFT/Scenes /CartFlow/Cart/CartViewController.swift
@@ -36,18 +36,77 @@ final class CartViewController: UIViewController {
         return table
     }()
     
+    private lazy var placeholderLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        label.text = NSLocalizedString("Cart is empty", comment: "")
+        return label
+    }()
+    
+    private lazy var payButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 16
+        button.backgroundColor = .label
+        button.setTitle(NSLocalizedString("Proceed to payment", comment: ""), for: .normal)
+        button.addTarget(self, action: #selector(payButtonTapped), for: .touchUpInside)
+        button.titleLabel?.textColor = .systemBackground
+        button.titleLabel?.font = .systemFont(ofSize: 17, weight: .bold)
+        return button
+    }()
+    
+    private lazy var totalNftsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 15, weight: .regular)
+        return label
+    }()
+    
+    private lazy var totalNftsPriceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(resource: .nftGreen)
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        return label
+    }()
+    
+    private lazy var labelsStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [totalNftsLabel, totalNftsPriceLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 2
+        return stack
+    }()
+    
+    private lazy var backgroundView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 12
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        view.backgroundColor = UIColor(resource: .nftLightGray)
+        return view
+    }()
+    
     private let cellReuseIdentifier = CartTableViewCell.reuseIdentifier
-    private let nfts: [Nft] = [
+    private var nfts: [Nft] = [
         Nft(name: "nigga", id: "1", images: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Piper/1.png")!], rating: 2, price: 1.78),
         Nft(name: "nigga", id: "1", images: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Piper/1.png")!], rating: 2, price: 1.78)
-    ]
+    ] {
+        didSet {
+            
+        }
+    }
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupNavigationBar()
         setupUI()
     }
     
@@ -65,15 +124,53 @@ final class CartViewController: UIViewController {
     }
     
     private func setupUI() {
-        view.addSubviews(cartTableView)
+        view.addSubviews(cartTableView, backgroundView)
+        backgroundView.addSubviews(labelsStackView, payButton)
         
         NSLayoutConstraint.activate([
             cartTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
             cartTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 20),
             cartTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            cartTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            cartTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            backgroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.heightAnchor.constraint(equalToConstant: 76),
+            
+            labelsStackView.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
+            labelsStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 16),
+            
+            payButton.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
+            payButton.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -16),
+            payButton.heightAnchor.constraint(equalToConstant: 44),
+            payButton.widthAnchor.constraint(equalToConstant: 240)
         ])
     }
+    
+    private func calculateCart() {
+        
+    }
+    
+    private func cartIsEmpty() {
+        cartTableView.removeFromSuperview()
+        navigationItem.rightBarButtonItem?.customView?.removeFromSuperview()
+
+        view.addSubview(placeholderLabel)
+        NSLayoutConstraint.activate([
+            placeholderLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            placeholderLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func cartNonEmpty() {
+        setupUI()
+        setupNavigationBar()
+    }
+    
+    // MARK: - OBJ-C methods
+    
+    @objc private func payButtonTapped() {}
     
     @objc private func sortButtonTapped() {}
 }
@@ -82,7 +179,12 @@ final class CartViewController: UIViewController {
 
 extension CartViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        nfts.count
+        guard !nfts.isEmpty else {
+            cartIsEmpty()
+            return 0
+        }
+        cartNonEmpty()
+        return nfts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
